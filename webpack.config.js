b5341ec6669bb597884a079aeaa5afa7fda472b5
David Emory <demory@conveyal.com>
2017-04-17 17:23:52 -0400

    Initial implementation of TriMet mobile-first UI design

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       exclude: [/node_modules/],
       use: [{
         loader: 'babel-loader',
-        options: { presets: ['es2015', 'react'] }
+        options: { presets: ['es2015', 'react', 'stage-0'] }
       }]
     },
       /*{
