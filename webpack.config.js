7fc61cb1f786bb1476d7135a29b6fd0ab479ca7e
David Emory <demory@conveyal.com>
2017-04-17 23:19:41 -0400

    Update webpack config; enable hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -4,60 +4,86 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   devtool: 'cheap-module-source-map',
+
   entry: [
+    // activate HMR for React
+    'react-hot-loader/patch',
+
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+    'webpack-dev-server/client?http://localhost:8080',
+
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+    'webpack/hot/only-dev-server',
+
     path.join(__dirname, 'lib/main.js')
   ],
+
   output: {
     path: path.join(__dirname, 'dist/'),
     filename: '[name].js',
-    publicPath: ''
+    publicPath: '/'
   },
+
+  devServer: {
+    // enable HMR on the server
+    hot: true,
+
+    // match the output path
+    contentBase: path.join(__dirname, 'dist/'),
+
+    // match the output `publicPath`
+    publicPath: '/'
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: 'lib/index.tpl.html',
       inject: 'body',
       filename: 'index.html'
     }),
-    //new webpack.optimize.OccurenceOrderPlugin(),
-    //new webpack.NoErrorsPlugin(),
+
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+
+    // enable HMR globally
+    new webpack.HotModuleReplacementPlugin(),
+
+    // prints more readable module names in the browser console on HMR updates
+    new webpack.NamedModulesPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: [/node_modules/],
-      use: [{
-        loader: 'babel-loader',
-        options: { presets: ['es2015', 'react', 'stage-0'] }
-      }]
-    },
-      /*{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    },*/ {
-      test: /\.json?$/,
-      loader: 'json'
-    },/* {
-      test: /\.yml$/,
-      loader: 'yaml-loader'
-    }, */{
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }, {
-      test: /\.png$/,
-      loader: 'url-loader?limit=100000'
-    }, {
-      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'file-loader?name=./assets/img/[hash].[ext]'
-    },{
-      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=./assets/fonts/[hash].[ext]'
-    },{
-      test: /\.jpg$/,
-      loader: 'file-loader'
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: 'babel-loader'
+      },
+      {
+        test: /\.json?$/,
+        use: 'json'
+      },
+      {
+        test: /\.css$/,
+        use: 'style-loader!css-loader'
+      },
+      {
+        test: /\.png$/,
+        use: 'url-loader?limit=100000'
+      }, {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'file-loader?name=./assets/img/[hash].[ext]'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff&name=./assets/fonts/[hash].[ext]'
+      },
+      {
+        test: /\.jpg$/,
+        use: 'file-loader'
+      }
+    ]
   }
 }
